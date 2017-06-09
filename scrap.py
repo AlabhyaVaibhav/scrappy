c1a541b40dc7dfc7ea979588a00c8095fd8cb42f
AlabhyaVaibhav <alabhya.vaibhav1997@gmail.com>
2017-06-09 23:44:31 +0530

    Scarppy Eg

--- scrap.py
+++ scrap.py
@@ -14,3 +14,24 @@ from bs4 import BeautifulSoup # $ pip install beautifulsoup4
 url = "YOUR_URL_HERE"
 soup = BeautifulSoup(urlopen(url))
 print(soup.get_text())
+
+'''Another code type'''
+from	urllib.request	import	urlopen 
+from	urllib.error	import	HTTPError 
+from	bs4	import	BeautifulSoup	
+def	getTitle(url):
+	try:
+		html	=	urlopen(url)
+	except	HTTPError	as	e:
+		return	None
+	try:
+		bsObj	=	BeautifulSoup(html.read())
+		title	=	bsObj.body.h1
+	except	AttributeError	as	e:
+		return	None
+	return	title
+title	=	getTitle("http://www.pythonscraping.com/exercises/exercise1.html") 
+if	title	==	None:
+	print("Title	could	not	be	found")
+else:
+	print(title)
