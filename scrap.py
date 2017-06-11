25546d69f5b85b4e72942f328a012d5e470eb6cc
AlabhyaVaibhav <alabhya.vaibhav1997@gmail.com>
2017-06-11 20:56:26 +0530

    Looping call added

--- scrap.py
+++ scrap.py
@@ -10,28 +10,8 @@ except ImportError:
     from urllib.request import urlopen # py3k
 
 from bs4 import BeautifulSoup # $ pip install beautifulsoup4
-
-url = "YOUR_URL_HERE"
-soup = BeautifulSoup(urlopen(url))
-print(soup.get_text())
-
-'''Another code type'''
-from	urllib.request	import	urlopen 
-from	urllib.error	import	HTTPError 
-from	bs4	import	BeautifulSoup	
-def	getTitle(url):
-	try:
-		html	=	urlopen(url)
-	except	HTTPError	as	e:
-		return	None
-	try:
-		bsObj	=	BeautifulSoup(html.read())
-		title	=	bsObj.body.h1
-	except	AttributeError	as	e:
-		return	None
-	return	title
-title	=	getTitle("http://www.pythonscraping.com/exercises/exercise1.html") 
-if	title	==	None:
-	print("Title	could	not	be	found")
-else:
-	print(title)
+html = 	urlopen("https://www.sulekha.com/school-tuitions/kolkata")
+bsObj	=	BeautifulSoup(html)
+nameList	=	bsObj.findAll("li",{"class":"list-item"}) 
+for	name in	nameList:
+	print(name.get_text())
